47754d00156081c57ac016e076187824e023a515
Rob Beardow <rbeardow@gmail.com>
2017-06-12 09:01:29 +1000

    Forgot gameboard.h

--- source/reversi_gameboard.h
+++ source/reversi_gameboard.h
@@ -9,19 +9,31 @@
 #include <string.h>
 #include <stdio.h>
 #include "reversi_shared.h"
+#include "reversi_helpers.h"
+
 #ifndef REVERSI_GAMEBOARD
 #define REVERSI_GAMEBOARD
-/* #defines for the size of the board */
+
+/* Defines for the size of the board */
 #define REVERSI_BOARDWIDTH 8
 #define REVERSI_BOARDHEIGHT REVERSI_BOARDWIDTH
-/* needed for the width of each cell when displaying */
+
+/* Needed for the width of each cell when displaying */
 #define REVERSI_CELLWIDTH 5
+
+
+#define REVERSI_VERTICAL_CELL_CHAR "|"
+#define REVERSI_HORIZONTAL_CELL_CHAR "-"
 
 /* colour constants for displaying the board */
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_BLUE "\x1b[34m"
 #define ANSI_COLOR_RESET "\x1b[0m"
 #define REVERSI_NUM_TOKEN_TYPES 3
+
+/* Defines the first player as CC_BLUE */
+#define REVERSI_FIRST_PLAYER 1
+
 enum reversi_cell_contents {
     CC_EMPTY, CC_BLUE, CC_RED
 };
@@ -37,7 +49,14 @@ typedef enum reversi_cell_contents
 struct reversi_coordinate{
     int x,y;
 };
+
 /* init the gameboard to the starting state - just copy the provided board */
 void reversi_gameboard_init(reversi_gameboard);
 void reversi_gameboard_display(reversi_gameboard);
+
+/*
+ * Draw a gameboard cell.
+ */
+void reversi_draw_cell(const char *, int, BOOLEAN);
+
 #endif
