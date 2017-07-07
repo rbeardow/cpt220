532672bf8c302fc700ad8a9a32ff90a2992c88c3
Rob Beardow <rbeardow@gmail.com>
2017-07-08 08:37:28 +1000

    Ensure input is freed in all branches.

--- source/reversi_helpers.c
+++ source/reversi_helpers.c
@@ -92,6 +92,7 @@ enum input_result request_string(char * msg, int length, char * string)
         {
             printf("Error: input string was too long.\n");
             read_rest_of_line();
+            free(input);
             return IR_FAILURE;
         }
         else
